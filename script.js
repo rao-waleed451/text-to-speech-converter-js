feb7224f21304b5b8af97783a172f02298eeaeba
rao-waleed451 <raowaleed451@gmail.com>
2025-10-08 07:56:15 +0500

    add the textarea and a new eventlistener which trigger on enter

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
-let speech=new SpeechSynthesisUtterance();
-let textarea=document.querySelector(".para")
-let button=document.querySelector(".btns")
-let voices=[];
-let voiceSelect=document.querySelector("select")
+let speech = new SpeechSynthesisUtterance;
+let textarea = document.querySelector(".para")
+let button = document.querySelector(".btns")
+let voices = [];
+let voiceSelect = document.querySelector("select")
 
-window.speechSynthesis.onvoiceschanged =()=>{
-    voices=window.speechSynthesis.getVoices();
-    speech.voice=voices[0]
+speechSynthesis.onvoiceschanged = () => {   // ya event listener tab trigger hota ha jab voices load ho jati hein
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0]    // yahan per default value humna 0 index wali add ker di
     //  textarea.innerHTML=""
-    speech.text=textarea.value
-    voices.forEach((voice,i)=>{
-        voiceSelect.options[i]= new Option(voice.name,i)
+    speech.text = textarea.innerHTML
+    voices.forEach((voice, i) => {
+        voiceSelect.options[i] = new Option(voice.name, i) // here in option i is the value in the html it will be eaual to their index new optiion aik tera method ha js ka ander jo option create karna ka liya use hota ha isma first option ha or dosra argument iski value ka liya ha
     })
 }
 
-voiceSelect.addEventListener("change",()=>{
-    speech.voice=voices[voiceSelect.value]
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value]    // humna new option bnata wagt her index iski value ma add kiya tha yani jo item 3 index per tha iski value ma 3 add kiyaa tha four per four
 })
 
 
- button.addEventListener("click",()=>{
-   speech.text=textarea.textContent;
+//  button.addEventListener("click",()=>{
+//     console.log("button is clicked")
+//    speech.text = textarea.innerText.trim();
+//    console.log(speech.text)
 
-   window.speechSynthesis.cancel();
+//    window.speechSynthesis.cancel();
 
-   window.speechSynthesis.speak(speech)
+//    window.speechSynthesis.speak(speech)
 
- })
+//  })
+
+button.addEventListener("click", () => {
+
+    setTimeout(() => {
+        speech.text = textarea.value
+        console.log(speech.text);
+
+        speechSynthesis.cancel();
+        speechSynthesis.speak(speech);
+    }, 0);
+});
+
+
+window.addEventListener("keypress",(e)=>{
+  if(e.key=="Enter"){
+    speech.text=textarea.value;
+    speechSynthesis.cancel();
+    speechSynthesis.speak(speech)
+  }
+})
